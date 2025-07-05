502b7c4c363ae39b3ce4adc965350e6a3ee2d3fa
William Mayorga <73685388+Kiliam84@users.noreply.github.com>
2025-07-04 23:05:28 -0500

    Create follow_the_gap directory
    Se creo el directorio para el paquete "follow_the_gap"

---
+++ src/follow_the_gap/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'follow_the_gap'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='william',
+    maintainer_email='william@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'follow_gap_f1tenth = follow_the_gap.follow_gap_f1tenth:main',
+            'lap_timer_node = follow_the_gap.lap_timer:main'
+        ],
+    },
+)
